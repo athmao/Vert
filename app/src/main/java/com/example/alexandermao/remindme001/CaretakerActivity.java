4bed8e523184603734757979addd042432d28ed4
Alexander Mao <mao@berkeley.edu>
2018-11-16 17:02:36 -0800

    made new activites

---
+++ app/src/main/java/com/example/alexandermao/remindme001/CaretakerActivity.java
@@ -0,0 +1,12 @@
+package com.example.alexandermao.remindme001;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CaretakerActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
